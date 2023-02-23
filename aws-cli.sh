0d0545d4963cf886fa8c4c11b0c4869be1db354f
Mohammad Jumah <mohammad_jumah@outlook.com>
2023-02-23 23:43:36 +0000

    adding instance role

--- aws-cli.sh
+++ aws-cli.sh
@@ -1,2 +1,3 @@
+aws cloudformation create-stack --stack-name instance-role --template-body file://instance-role.yaml --capabilities CAPABILITY_NAMED_IAM
 aws cloudformation create-stack --stack-name virtual-network --template-body file://virtual-network.yaml
 aws cloudformation create-stack --stack-name instance --template-body file://instance.yaml --parameters ParameterKey=myKeyPair,ParameterValue=website
